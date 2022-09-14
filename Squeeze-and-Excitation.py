798314208aa55c6d57ba1a0f9b82b57861db92ec
SZU-Bio-Lwp <113423865+SZU-Bio-Lwp@users.noreply.github.com>
2022-09-14 17:13:01 +0800

    Add files via upload

---
+++ Squeeze-and-Excitation.py
@@ -0,0 +1,20 @@
+from tensorflow.keras.layers import GlobalAveragePooling2D, Reshape, Dense, Permute, multiply
+import tensorflow.keras.backend as K
+
+
+def squeeze_excite_block(tensor, ratio=16):
+    init = tensor
+    channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+    filters = init.shape[channel_axis]
+    se_shape = (1, 1, filters)
+
+    se = GlobalAveragePooling2D()(init)
+    se = Reshape(se_shape)(se)
+    se = Dense(filters // ratio, activation='relu', kernel_initializer='he_normal', use_bias=False)(se)
+    se = Dense(filters, activation='sigmoid', kernel_initializer='he_normal', use_bias=False)(se)
+
+    if K.image_data_format() == 'channels_first':
+        se = Permute((3, 1, 2))(se)
+
+    x = multiply([init, se])
+    return x
